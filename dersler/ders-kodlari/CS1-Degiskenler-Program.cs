9ddd35056364df36e0bd3913ba518e82992e428d
Baran Taylan <bataylan@live.com>
2020-12-23 16:21:26 +0300

    Update CS1-Degiskenler-Program.cs

--- dersler/ders-kodlari/CS1-Degiskenler-Program.cs
+++ dersler/ders-kodlari/CS1-Degiskenler-Program.cs
@@ -6,40 +6,43 @@ namespace SEG_Example_Console
     {
         static void Main(string[] args)
         {
-            //Terminale yazi yazdirma
-            Console.WriteLine("SEG Ekibi CS1 Degisken Turleri - Tarih: 23.12.2020");
+            ////Terminale yazi yazdirma
+            Console.WriteLine("SEG Ekibi CS2 Degisken Turleri - Tarih: 23.12.2020");
 
             Console.WriteLine("SEG Ekibi CSharp Temel Ilk Ders - Tarih: 23.12.2020");
 
-            //SIK kullanacagimiz degiskenler
-            //String (yazi) degiskeni
+            ////String (yazi) degiskeni
             string ekipIsmi = "SEG Ekibi";
             string tarihYazisi = "Tarih: 23.12.2020";
 
-            Console.WriteLine(ekipIsmi + " CS1 Ekrana Yazdirma - " + tarihYazisi);
+            Console.WriteLine(ekipIsmi + " CS2 Degisken Turleri - " + tarihYazisi);
 
             Console.WriteLine(ekipIsmi + " CSharp Temel Ilk Ders - " + tarihYazisi);
 
-            //Integer (Tam sayi) degiskeni
-            //Hayali karakter hakkinda yazdirma
-            string karakterIsmi = "Fatih ";
-            int karakterYasi = 24;
+            //Integer (tam sayi) degiskenler
+            //Hayali karakter
+            string karakterIsmi = "Ziya";
+            int karakterYasi = 25;
 
-            Console.WriteLine(karakterIsmi + karakterYasi + " yasinda.");
-            Console.WriteLine("Ama " + karakterIsmi + "'in gosterdigi yas: " + (karakterYasi - 10));
-            Console.WriteLine("Hayata bakis acisiysa " + (karakterYasi + 20) + "yasinda birisine ait olmali.");
+            Console.WriteLine(karakterIsmi + " " + karakterYasi + " yasinda. " + tarihYazisi);
 
-            //Hayali karakterimiz 3 yil sonra kac yasinda olacak
+            Console.WriteLine(karakterIsmi + " yasini 5 yas buyuk soyluyor. Soyledigi yas:" + (karakterYasi + 5));
+
+            //3 yil sonraki yasini yazdiralim
             karakterYasi = karakterYasi + 3;
-            Console.WriteLine(karakterIsmi +"3 yil sonra " + karakterYasi + " yasinda olacak.");
+            Console.WriteLine(karakterIsmi + " 3 yil sonra " + karakterYasi + " yasinda olacak.");
 
-            //Boolean (Mantiksal) Degiskeni
-            //bool dogru ve yanlisi bilgisini tutar
-            bool fatihCocukMu = false; 
-            bool fatihYetiskinMi = true;
+            //5 yil sonraki yasini yazdiralim
+            karakterYasi = karakterYasi + 2;
+            Console.WriteLine(karakterIsmi + " 5 yil sonra " + karakterYasi + " yasinda olacak.");
+
+            //Boolean (Mantiksal) Degisken
+            //bool dogru (true) ya da yanlis (false) bilgisini tutar
+            bool karakterCocukMu = false;
+            bool karakterYetiskinMi = true;
 
             //Float (noktali sayi) Degiskeni
-            float yercekimiIvmesi = 9.81f;
+            float yercekimiIvmesi = 9.18f;
             
         }
     }
