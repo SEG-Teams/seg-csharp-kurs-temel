f4222a66f98d0d1ae1ca2d30f00f22f5425db767
Baran <bataylan@live.com>
2020-12-23 12:04:21 +0300

    Add files via upload

---
+++ egzersizler/sizden-gelenler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SEG_Example_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Bu benim ilk C# programim!");
+
+            Console.WriteLine("2. yazdirilan satir.");
+
+            Console.WriteLine("3. yazdirilan satir.");
+
+            Console.WriteLine("4. yazdirilan satir.");
+        }
+    }
+}
